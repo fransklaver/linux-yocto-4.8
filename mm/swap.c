fd8ca52060753cdb7674f0d756d0155b595445de
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-10-18 14:30:17 -0400

    rt: swap relocate add_drain code further down into the file
    ...to align with upstream v4.8-rt.  No significant functional changes.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- mm/swap.c
+++ mm/swap.c
@@ -683,26 +683,11 @@ static inline void remote_lru_add_drain(int cpu, struct cpumask *has_work)
 
 #else
 
-static void lru_add_drain_per_cpu(struct work_struct *dummy)
-{
-	lru_add_drain();
-}
-
-static DEFINE_PER_CPU(struct work_struct, lru_add_drain_work);
-static struct workqueue_struct *lru_add_drain_wq;
-static inline void remote_lru_add_drain(int cpu, struct cpumask *has_work)
-{
-	struct work_struct *work = &per_cpu(lru_add_drain_work, cpu);
-
-	INIT_WORK(work, lru_add_drain_per_cpu);
-	queue_work_on(cpu, lru_add_drain_wq, work);
-	cpumask_set_cpu(cpu, has_work);
-}
-
 /*
  * lru_add_drain_wq is used to do lru_add_drain_all() from a WQ_MEM_RECLAIM
  * workqueue, aiding in getting memory freed.
  */
+static struct workqueue_struct *lru_add_drain_wq;
 
 static int __init lru_init(void)
 {
@@ -716,7 +701,21 @@ static int __init lru_init(void)
 }
 early_initcall(lru_init);
 
-#endif /* !RT_BASE */
+static void lru_add_drain_per_cpu(struct work_struct *dummy)
+{
+	lru_add_drain();
+}
+
+static DEFINE_PER_CPU(struct work_struct, lru_add_drain_work);
+static inline void remote_lru_add_drain(int cpu, struct cpumask *has_work)
+{
+	struct work_struct *work = &per_cpu(lru_add_drain_work, cpu);
+
+	INIT_WORK(work, lru_add_drain_per_cpu);
+	queue_work_on(cpu, lru_add_drain_wq, work);
+	cpumask_set_cpu(cpu, has_work);
+}
+#endif
 
 void lru_add_drain_all(void)
 {
